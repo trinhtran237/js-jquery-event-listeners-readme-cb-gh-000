ef19ef76c0236b6fe3d1fec1ef34c578b87475ee
Diep Thuy <boychaos9x@gmail.com>
2020-05-13 17:28:50 +0000

    Automatically backed up by Learn

--- js/events.js
+++ js/events.js
@@ -3,5 +3,12 @@
 $(document).ready(function(){
 
 // call functions here
-
+$("form").on("submit", function() {
+  if ($( "input:first" ).val() === "correct") {
+    alert('your form is going to be submitted now');
+    return;
+  }
+  alert("you entered the wrong value");
+  return;
+});
 });
